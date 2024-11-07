e54afb355c4fe6771d25b4f476e69b7151832e59
OctavioAR <159207322+OctavioAR@users.noreply.github.com>
2024-11-06 18:02:33 -0700

    Add files via upload

---
+++ UsoDeApi/js/personas.js
@@ -0,0 +1,53 @@
+const url='https://randomuser.me/api/?results=12';
+let buton=document.querySelector("button");
+let lista=document.querySelector("#lista-personas");
+//let foto=document.querySelector("#foto");
+//let nombre=document.querySelector("#nombre");
+//let direccion=document.querySelector("#direccion");
+//let telefono=document.querySelector("#telefono");
+
+
+const agregarPersonasLista = (persona) =>{
+    lista.classList.add("lista-personas");
+    const tarjeta = document.createElement("article");
+    tarjeta.classList.add("tarjeta");
+
+    const foto=document.createElement("img");
+    foto.setAttribute("src",persona.picture.large);
+
+    const nombre=document.createElement("article");
+    nombre.textContent=persona.name.first+" "+persona.name.last;
+    const direccion=document.createElement("article");
+    direccion.textContent=persona.location.street.name+" "+persona.location.street.number;
+    const telefono=document.createElement("article");
+    telefono.textContent=persona.phone;
+
+    tarjeta.appendChild(foto);
+    tarjeta.appendChild(nombre);
+    tarjeta.appendChild(direccion);
+    tarjeta.appendChild(telefono);
+    
+    lista.appendChild(tarjeta);
+}    
+
+
+const obtenerPersona = () =>{
+    //limpio la lista de personas
+    lista.innerHTML = "";
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(respuesta => {
+            respuesta.results.forEach(persona => {
+                agregarPersonasLista(persona);
+                /*nombre.textContent=persona.name.first+" "+persona.name.last; 
+                foto.setAttribute("src",persona.picture.large);  
+                direccion.textContent=persona.location.street.name+" "+persona.location.street.number;
+                telefono.textContent=persona.phone;*/
+            });
+        })
+}
+
+buton.addEventListener("click", () => {
+    obtenerPersona();
+});
+obtenerPersona();
